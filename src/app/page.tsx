3b13e061e94d92f3bb2822cacfbd4962585e77dc
N.I <iworksWeb@gmail.com>
2024-12-14 12:33:29 +0900

    init microcms fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import Image from "next/image";
+import { client } from "./libs/client";
 
-export default function Home() {
+export default async function Home() {
+  const data = await client.get({
+    endpoint: "articles",
+  });
+  console.log(data);
+  console.log(data.contents[1].title);
   return (
     <div>
       <div>Hello</div>
