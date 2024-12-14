5a76bb4a2805f5225de905dc1800f6aab7e8b447
N.I <iworksWeb@gmail.com>
2024-12-14 14:13:24 +0900

    add background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import styles from "./styles/Home.module.css";
+
 export default async function Home() {
   return (
-    <div>
-      <div>Hello</div>
-    </div>
+    <>
+      <div className={`${styles.animatedBackground} h-44 -mb-40  w-full`}></div>
+      <main className="container mx-auto px-2 py-12">
+        <div className="relative">
+          <h2 className="text-center md:text-5xl z-20 relative text-white">
+            Iwa-Media
+          </h2>
+        </div>
+      </main>
+    </>
   );
 }
