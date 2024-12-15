407a830655786404cc061e04f6dcb48ca2816349
N.I <iworksWeb@gmail.com>
2024-12-15 14:39:25 +0900

    add detail page title createAt tag

--- src/app/articles/[contentid]/page.tsx
+++ src/app/articles/[contentid]/page.tsx
@@ -1,11 +1,36 @@
 import { getDetailArticles } from "@/app/libs/api";
+import Image from "next/image";
 import React from "react";
 
 const DetailArticle = async ({ params }: { params: { contentId: string } }) => {
   const contentId = params.contentId;
   const { data } = await getDetailArticles(contentId);
-  console.log(data);
-  return <div>Details</div>;
+  const { title, thumbnails, createdAt, content, author, tags } = data;
+  const tagList = Array.isArray(tags) ? tags : [tags];
+
+  console.log("tags:", tags);
+  return (
+    <div className="container mx-auto md:max-w-[760px] my-4">
+      <Image src={thumbnails.url} alt="title" width={760} height={420} />
+      <div className="mx-auto text-center">
+        <h2 className="font-semibold my-4 md:text-4xl">{title}</h2>
+        <div className="my-2 text-gray-500">
+          <span>{new Date(createdAt).toLocaleDateString()}</span>
+          <span>著者{author}</span>
+        </div>
+        <div className="my-4 inline-block">
+          {tagList.map((tag: { name: string }) => (
+            <div
+              key={tag.name}
+              className="border-2 px-2 py-1 rounded-md border-slate-500"
+            >
+              <span className="text-sm">{tag.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DetailArticle;
--- src/app/libs/api.ts
+++ src/app/libs/api.ts
@@ -1,4 +1,3 @@
-import { Article } from "../types/types";
 import { client } from "./client";
 
 /**
