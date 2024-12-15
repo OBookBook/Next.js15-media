852491c4a84f3c07c342e0e4e36edc1b6c0bac93
N.I <iworksWeb@gmail.com>
2024-12-15 22:08:32 +0900

    create Hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import Hero from "@/components/layout/Hero";
 import ArticleCardList from "../components/ArticleList";
 import LoadMoreButton from "../components/LoadMoreButton";
 import { getAllArticles } from "./libs/api";
-import styles from "./styles/Home.module.css";
 
 export default async function Home() {
   const { data } = await getAllArticles(0, 2);
@@ -9,16 +9,8 @@ export default async function Home() {
   const totalCount = data.totalCount;
   return (
     <>
-      <div
-        className={`${styles.animatedBackground} h-44 -mb-40 relative w-full`}
-      ></div>
       <main className="mx-auto px-2 py-12">
-        <div className="relative">
-          <h2 className="text-center md:text-5xl z-20 relative text-white">
-            Iwa-Media
-          </h2>
-        </div>
-
+        <Hero title="メディア" />
         <div className="mt-24">
           <div className="mx-auto text-center py-4">
             <h2 className="md:text-4xl">記事一覧</h2>
--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "@/components/layout/Hero";
 import React from "react";
 
-const DetailTag = () => {
-  return <div>DetailTag</div>;
+const DetailTag = ({ params }: { params: { slug: string } }) => {
+  const slug = params.slug;
+
+  return (
+    <div className="px-2 py-12">
+      <Hero title={slug} />
+    </div>
+  );
 };
 
 export default DetailTag;
